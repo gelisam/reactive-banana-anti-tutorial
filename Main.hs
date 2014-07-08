0b847ac64f92dfdd81b3fa6c82524f93c9869c4f
Samuel Gélineau <gelisam+github@gmail.com>
2014-07-07 23:41:12 -0400

    play version

--- Main.hs
+++ Main.hs
@@ -4,4 +4,10 @@ import Graphics.Gloss
 
 
 main :: IO ()
-main = display (InWindow "Nice Window" (200, 200) (800, 200)) white (Circle 80)
+main = play (InWindow "Nice Window" (200, 200) (800, 200))
+            white
+            30
+            ()
+            (\() -> Circle 80)
+            (\_ () -> ())
+            (\_ () -> ())
